f49a656d68443564b292467a1ee243b123aaa639
Prashant Jain <praz.jain@gmail.com>
2024-04-18 12:43:05 -0400

    initial commit

---
+++ web-api-kafka-producer/src/main/java/com/prazjain/webapikafkaproducer/WebApiKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.prazjain.webapikafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ web-api-kafka-producer/src/test/java/com/prazjain/webapikafkaproducer/WebApiKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prazjain.webapikafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
